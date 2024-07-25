8c2c72bc4c53211f0bda96e001406d85a46be27c
kisun-sah <kishunsah783@gmail.com>
2024-07-25 11:54:22 +0530

    add new code

---
+++ BackEnd_code/server.js
@@ -0,0 +1,9 @@
+var slugify = require('slugify')
+
+const a =slugify('some string') // some-string
+
+console.log(a);
+// if you prefer something other than '-' as separator
+const b = slugify('some string', '_')  // some_string
+
+console.log(b);
